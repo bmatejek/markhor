5eda9e4ab22e3f55468c71de4c989ae5074ac95a
bmatejek <bmatejek@gmail.com>
2020-03-04 15:48:59 -0500

    dataIO: created method for reading compressed files into an array

--- utilities/dataIO.py
+++ utilities/dataIO.py
@@ -1,8 +1,13 @@
+import glob
 import h5py
 
 
 
 import numpy as np
+
+
+
+from PIL import Image
 
 
 
@@ -12,3 +17,28 @@ def ReadH5File(prefix):
         data = np.array(hf[list(hf.keys())[0]]).astype(np.uint8)
 
     return data
+
+
+
+def ReadPNGFiles(prefix):
+    compressed_files = sorted(glob.glob('images/{}/*.png'.format(prefix)))
+
+    for iz, filename in enumerate(compressed_files):
+        img = np.array(Image.open(filename)).astype(np.uint8)
+
+        if not iz:
+            zres = len(compressed_files)
+            yres = img.shape[0]
+            xres = img.shape[1]
+
+            data = np.zeros((zres, yres, xres), dtype=np.uint8)
+
+        data[iz,:,:] = img
+
+    return data
+
+
+
+def ReadCompressedFiles(prefix, scheme):
+    if scheme == 'png': return ReadPNGFiles(prefix)
+    else: assert (False)
